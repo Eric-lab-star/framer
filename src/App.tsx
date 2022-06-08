1d608843d5541eef12b123af9c5a05b6a1627175
Eric Kim <cyon256@icloud.com>
2022-06-08 21:18:12 +0900

    animate exit

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,55 @@
-import React, { useEffect, useRef } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import styled from "styled-components";
-import { motion } from "framer-motion";
+import { AnimatePresence, motion } from "framer-motion";
 
 const Wrapper = styled.div`
   height: 100vh;
   width: 100vw;
   display: flex;
+  flex-direction: column;
   justify-content: center;
   align-items: center;
 `;
 
-const Svg = styled(motion.svg)`
-  width: 150px;
+const Container = styled(motion.div)`
   height: 100px;
+  width: 100px;
+  background-color: white;
+  border-radius: 15px;
+  margin-bottom: 200px;
+  position: absolute;
 `;
 
-const svgVariants = {
+const boxVariant = {
   start: {
-    fill: "transparent",
-    pathLength: 0,
-    stroke: "rgba(255,255,255,1)",
-    strokeWidth: 6,
+    scale: 1,
   },
-  end: { pathLength: 1, stroke: "#feee34", transition: { duration: 4 } },
+  end: {
+    scale: 1.5,
+    rotateZ: 90,
+    transition: {
+      duration: 1,
+      type: "spring",
+      bounce: 0.5,
+    },
+  },
 };
 
 function App() {
-  return <Wrapper></Wrapper>;
+  const [showing, setShowing] = useState(false);
+  const onClick = () => {
+    setShowing((prev) => !prev);
+  };
+  return (
+    <Wrapper>
+      <AnimatePresence>
+        {showing ? (
+          <Container variants={boxVariant} initial="start" animate="end" />
+        ) : null}
+      </AnimatePresence>
+      <button onClick={onClick}>Click</button>
+    </Wrapper>
+  );
 }
 
 export default App;
